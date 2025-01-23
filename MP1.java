a26c248ceebb49f69043dd08eee45b10c446bb38
bfridkis <bfridkis@gmail.com>
2025-01-22 19:34:28 -0600

    updates to MP1.java

--- MP1.java
+++ MP1.java
@@ -35,13 +35,82 @@ public class MP1 {
         }
         return ret;
     }
+	
+	// Taken directly from top solution: https://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values
+	public class MapUtil {
+		public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+			List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+			list.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+
+			Map<K, V> result = new LinkedHashMap<>();
+			for (Map.Entry<K, V> entry : list) {
+				result.put(entry.getKey(), entry.getValue());
+			}
+
+			return result;
+    }
+}
 
     public String[] process() throws Exception{
     	String[] topItems = new String[20];
         Integer[] indexes = getIndexes();
 
     	//TO DO
+		// Enter data using BufferReader
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 
+		int lineCount = 0;
+		String[] titles = new String[50000];
+		String s = r.readLine();
+		//while(r.ready()) {
+		while(s != null) {	
+			// Testing only
+			//String s = r.readLine() + " " + lineCount;
+			//System.out.println(s);
+			
+			// Reading data using readLine and store in titles array
+			//String s = r.readLine();
+			titles[lineCount++] = s;
+			//System.out.println(titles[lineCount-1] + " " + (lineCount - 1));
+			//System.out.flush();
+			s = r.readLine();
+		}
+		r.close();
+		// System.out.println("*****************************************");
+		// System.out.println("");
+		Map <String, Integer> wordCounts = new HashMap<>();
+		for(int index : indexes) {
+			//tokenizing title based on delimiters
+			//System.out.println("index: " + index + " " + titles[index]);
+			//String[] words = titles[index].split(delimiters);
+			String[] words = titles[index].split("[ \t,;\\.\\?\\!\\-:@\\[\\]\\(\\)\\{\\}_*/]");
+			//String[] words = titles[index].split(Pattern.quote(delimiters));
+			//count words in the title, ignoring words in stopWordsArray
+			for(String word : words) {
+				String wordNWSLC = word.toLowerCase().replaceAll("\\s+","");
+				if(wordNWSLC != "" && !Arrays.asList(stopWordsArray).contains(wordNWSLC) && wordCounts.containsKey(wordNWSLC)) {
+					wordCounts.put(wordNWSLC, wordCounts.get(wordNWSLC) + 1);
+				}
+				else if (wordNWSLC != "" && !Arrays.asList(stopWordsArray).contains(wordNWSLC)) {
+					wordCounts.put(wordNWSLC, 1);
+				}
+			}
+		}
+		
+		/* for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		} */
+		
+		Map<String, Integer> sortedWordCounts = MapUtil.sortByValue(wordCounts);
+		
+		int returnCount = 0;
+		for (Map.Entry<String, Integer> entry : sortedWordCounts.entrySet()) {
+			if(returnCount == 20) {
+				break;
+			}
+			//System.out.println(entry.getKey() + " " + entry.getValue());
+			topItems[returnCount++] = entry.getKey();
+		}
 
 		return topItems;
     }
@@ -62,3 +131,21 @@ public class MP1 {
 	}
 
 }
+
+// REFERENCES
+// https://www.geeksforgeeks.org/ways-to-read-input-from-console-in-java/
+// https://www.geeksforgeeks.org/bufferedreader-readline-method-in-java-with-examples/
+// https://stackoverflow.com/questions/5071040/java-convert-integer-to-string
+// https://stackoverflow.com/questions/5455794/removing-whitespace-from-strings-in-java
+// https://www.google.com/search?q=make+lowercase+java&rlz=1C1CHBF_enUS1071US1071&oq=make+lowercase+java&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDIHCAcQABiABDIHCAgQABiABDIHCAkQABiABNIBCDI1NDBqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8
+// https://stackoverflow.com/questions/1348199/what-is-the-difference-between-the-hashmap-and-map-objects-in-java\
+// https://stackoverflow.com/questions/4157972/how-to-update-a-value-given-a-key-in-a-hashmap
+// https://www.w3schools.com/jsref/jsref_includes_array.asp
+// https://www.geeksforgeeks.org/sorting-a-hashmap-according-to-values/
+// https://stackoverflow.com/questions/59924699/for-loops-and-arrays-in-java
+// https://stackoverflow.com/questions/1128723/how-do-i-determine-whether-an-array-contains-a-particular-value-in-java
+// https://www.google.com/search?q=iterating+through+first+x+elements+of+a+map+java&rlz=1C1CHBF_enUS1071US1071&oq=iterating+through+first+x+elements+of+a+map+java&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORigATIHCAEQIRigATIHCAIQIRifBTIHCAMQIRifBdIBCDcxMTJqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8
+// https://stackoverflow.com/questions/14134558/list-of-all-special-characters-that-need-to-be-escaped-in-a-regex
+// https://medium.com/sina-ahmadi/java-regex-6e4d073aab85
+// https://stackoverflow.com/questions/53325650/what-is-the-type-of-map-entry-comparingbyvalue-reversed
+// https://stackoverflow.com/questions/79379449/reading-from-stdin-via-bufferedreader-in-while-loop-array-only-correct-if-syst
